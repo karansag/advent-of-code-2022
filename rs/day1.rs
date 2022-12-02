88c6ca4a49f8230e8aa278da5f6b99421ebbee7d
Karan Sagar <ksagar1030@gmail.com>
2022-12-01 23:07:04 -0500

    [Day 1] [Rust] Day 1 part 2 solution

--- rs/day1.rs
+++ rs/day1.rs
@@ -15,7 +15,9 @@ fn read_file(file_name: &str) -> Result<Vec<Option<i32>>, std::io::Error> {
 
 fn main() -> Result<(), std::io::Error> {
     let contents = read_file("../inputs/day-1-input.txt")?;
-    let vec_of_vecs = contents.split(|elt| elt.is_none()).map(|elt| elt.iter().map(|e| e.unwrap()).sum::<i32>());
-    println!("{}", vec_of_vecs.max().unwrap());
+    let mut vec_of_vecs: Vec<i32> = contents.split(|elt| elt.is_none()).map(|elt| elt.iter().map(|e| e.unwrap()).sum::<i32>()).collect();
+    vec_of_vecs.sort_by(|a, b| b.cmp(a));
+    let result: Vec<&i32> = vec_of_vecs.iter().take(3).collect();
+    println!("{:?}", result);
     Ok(())
 }
