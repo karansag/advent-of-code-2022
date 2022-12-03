6195c5bb32479464d466848198d1784f3fb7e1a9
Karan Sagar <ksagar1030@gmail.com>
2022-12-03 00:53:06 -0500

    [Day 2] [Rust] Part 2

--- rs/day2.rs
+++ rs/day2.rs
@@ -18,19 +18,33 @@ fn compare(a: &str, b: &str) -> i32 {
     }
 }
 
+fn get_my_choice<'a>(opponent_choice: &'a str, desired_outcome: &'a str) -> &'a str {
+    let comparison = ["A", "B", "C"];
+    let pos_opponent = comparison.iter().position(|&x| x == opponent_choice).unwrap();
+    return match (opponent_choice, desired_outcome) {
+        (y, "X") => comparison[(pos_opponent + 2) % 3],
+        (x, "Y") => x,
+        (y, "Z") => comparison[(pos_opponent + 1) % 3],
+            _ => todo!()
+    }
+}
 
 fn handle_pair(mut p: std::str::Split<&str>) -> i32 {
-    let conversion: HashMap<&str, &str> = [
-        ("X", "A"), ("Y", "B"), ("Z", "C")
-    ].iter().cloned().collect();
-    let pair = (p.next().unwrap(), conversion.get(p.next().unwrap()).unwrap());
-    let my_choice_score = match pair.1 {
-        &"A" => 1,
-        &"B" => 2,
-        &"C" => 3,
+    // Part 1 commented out
+    // let conversion: HashMap<&str, &str> = [
+    //     ("X", "A"), ("Y", "B"), ("Z", "C")
+    // ].iter().cloned().collect();
+    // let pair = (p.next().unwrap(), conversion.get(p.next().unwrap()).unwrap());
+    // Part 2
+    let pair = (p.next().unwrap(), p.next().unwrap());
+    let my_choice = get_my_choice(pair.0, pair.1);
+    let my_choice_score = match my_choice {
+        "A" => 1,
+        "B" => 2,
+        "C" => 3,
         _  => todo!()
     };
-    let result_score = (compare(pair.1, pair.0) + 1) * 3;
+    let result_score = (compare(my_choice, pair.0) + 1) * 3;
     return my_choice_score + result_score;
 
 }
@@ -39,6 +53,7 @@ fn handle_pair(mut p: std::str::Split<&str>) -> i32 {
 fn main() -> Result<(), std::io::Error> {
     let contents = utils::read_file("../inputs/day-2-input.txt")?;
     let total: i32 = contents
+        // .take(10)
         .map(|p| handle_pair(p.unwrap().split(" "))).sum();
     println!("total: {:?}", total);
     Ok(())
