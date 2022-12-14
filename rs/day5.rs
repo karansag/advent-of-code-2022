550389308d3a7a686daef8d1b72a695459135f8a
Karan Sagar <ksagar1030@gmail.com>
2022-12-14 00:51:01 -0500

    [Day 5] [Rust] Part 1 and inputs

---
+++ rs/day5.rs
@@ -0,0 +1,108 @@
+mod utils;
+
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Stack {
+    st: Vec<char>,
+}
+
+impl Stack {
+    fn pop(&mut self) -> Option<char> {
+        self.st.pop()
+    }
+    fn push(&mut self, item: char) -> () {
+        self.st.push(item)
+    }
+}
+
+struct Instruction {
+    from: usize,
+    to: usize,
+    quantity: u16,
+}
+
+fn parse_stacks(items: &[String]) -> Vec<Stack> {
+    fn _tokenize_row(row_string: &String) -> Vec<Option<char>> {
+        let values = utils::split_every(row_string, 3);
+        values.iter().map(|v| {
+            if v.starts_with('[') {
+                return Some(v.chars().nth(1).unwrap());
+            } else {
+                return None;
+            }
+        }).collect()
+    }
+    let tokens = utils::transpose(items.iter().map(_tokenize_row).collect());
+    let mut r: Vec<Stack> = Vec::with_capacity(tokens.len());
+    // let row_length = tokens[0].len();
+    for row in tokens {
+        let mut new_stack = Stack { st: vec![] };
+        for item in row.iter().rev() {
+            if let Some(character) = item {
+                new_stack.push(*character)
+            }
+        }
+        r.push(new_stack);
+    }
+    r
+}
+
+fn parse_stack_section(stack_text: Vec<String>) -> Vec<Stack> {
+    let (all_but_last, last_line_vec) = utils::split_slice(&stack_text, stack_text.len() - 1);
+    let count = last_line_vec[0]
+        .split_whitespace()
+        .map(u8::from_str)
+        .count();
+    let items = parse_stacks(all_but_last);
+    items
+}
+
+fn parse_instructions(inst_text: Vec<String>) -> Vec<Instruction> {
+    fn parse_inst(t: &String) -> Instruction {
+        let words: Vec<&str> = t.split_whitespace().collect();
+        Instruction {
+            quantity: u16::from_str(words[1]).unwrap(),
+            from: usize::from_str(words[3]).unwrap(),
+            to: usize::from_str(words[5]).unwrap(),
+        }
+    }
+    inst_text.iter().map(parse_inst).collect()
+}
+
+fn apply_instructions(stacks: &mut Vec<Stack>, instructions: &Vec<Instruction>) -> () {
+    for inst in instructions {
+        let from_ind = inst.from - 1;
+        let to_ind = inst.to - 1;
+        for _ in 0..inst.quantity {
+            let ch = stacks[from_ind].pop().unwrap();
+            stacks[to_ind].push(ch);
+        }
+    }
+    ()
+}
+
+fn parse_data(s: &str) -> Result<(Vec<Stack>, Vec<Instruction>), std::io::Error> {
+    let mut file_contents = utils::read_file(s)?;
+    let stack_text = file_contents
+        .by_ref()
+        .map(|l| l.unwrap())
+        .take_while(|l| !l.is_empty())
+        .collect();
+    let instruction_text = file_contents.map(|l| l.unwrap()).collect();
+    let stacks: Vec<Stack> = parse_stack_section(stack_text);
+    let insts: Vec<Instruction> = parse_instructions(instruction_text);
+
+    Ok((stacks, insts))
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let (mut stacks, insts) = parse_data("../inputs/day-5-input.txt")?;
+    apply_instructions(&mut stacks, &insts);
+    for s in stacks {
+        println!("{:?}", s);
+    }
+    // execute_instructions(stacks, instructions);
+    // let top_items: Vec<String> = read_top_items(stacks);
+    Ok(())
+}
--- rs/utils.rs
+++ rs/utils.rs
@@ -9,3 +9,33 @@ where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
+
+pub fn split_slice<P>(items: &[P], ind: usize) -> (&[P], &[P]) {
+    (&items[..ind], &items[ind..])
+
+}
+
+pub fn split_every(s: &str, i: usize) -> Vec<String> {
+    let chars: Vec<char> = s.chars()
+        .enumerate()
+        .filter(|(ind, x)| ind % (i + 1) != i)
+        .map(|(ind, x)| x)
+        .collect();
+    chars.chunks(i).map(|c| c.iter().collect::<String>()).collect()
+}
+
+
+pub fn transpose<T: Copy>(m: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let mut t = vec![Vec::with_capacity(m.len()); m[0].len()];
+    for r in m {
+        for i in 0..r.len() {
+            t[i].push(r[i]);
+        }
+    }
+    t
+}
+
+
+fn main() -> Result<(), std::io::Error> {
+    Ok(())
+}
