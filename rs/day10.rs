663dee3bd54672214e9f9ad3bcd0a1f8928a27d7
Karan Sagar <karansag@fb.com>
2022-12-24 09:58:53 -0500

    [Rust] [Day 10] Part 2

--- rs/day10.rs
+++ rs/day10.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use std::str::FromStr;
+use std::collections::HashMap;
 
 #[derive(PartialEq, Debug)]
 enum Inst {
@@ -17,11 +18,13 @@ fn parse_line(s: String) -> Inst {
     }
 }
 
-fn process_insts(insts: &Vec<Inst>, reg_start: i32) -> i32 {
+fn process_insts<'a>(insts: &Vec<Inst>, reg_start: i32, cycle_map: &'a mut HashMap<i32, i32>) -> (i32,  &'a HashMap<i32, i32>){
     let mut accum = 0;
     let mut cycle = 1;
     let mut reg = reg_start;
+    cycle_map.insert(1, reg_start);
     for inst in insts {
+        cycle_map.insert(cycle, reg);
         let cymod = cycle % 40;
         if cymod == 20 {
             println!("Value at cycle {:?} is {:?}", cycle, reg);
@@ -35,18 +38,49 @@ fn process_insts(insts: &Vec<Inst>, reg_start: i32) -> i32 {
                 println!("Value at cycle {:?} (addx) is {:?}", cycle + 1, reg);
                 accum += (cycle + 1) * reg;
             }
+            cycle_map.insert(cycle + 1, reg);
             cycle += 2;
             reg += val;
         }
     }
-    accum
+    (accum, cycle_map)
+}
+fn draw_screen(values: &Vec<bool>, width: i32) -> () {
+    let chars: Vec<char> = values.iter().map(|x| match x {
+        true => '#',
+        false => '.',
+    }).collect();
+    let results: Vec<String> = chars
+        .chunks(width as usize)
+        .map(|c| c.iter().collect::<String>())
+        .collect();
+
+    for line in results {
+        println!("{}", line);
+    }
 }
 
+fn calculate_pixels(width: i32, height: i32, pos_map: &HashMap<i32, i32>) -> Vec<bool> {
+    let mut ret = vec![false; (width * height) as usize];
+
+    for pix in 0..ret.len() {
+        let p = pix as i32;
+        let sprite_center = pos_map[&(p + 1)];
+        if ((p % width) - sprite_center).abs() < 2 {
+            ret[pix] = true;
+        }
+    }
+    ret
+
+}
 fn main() -> Result<(), std::io::Error> {
     let contents = utils::read_file("../inputs/day-10-input.txt")?;
     let insts: Vec<Inst> = contents.map(|l| l.unwrap()).map(parse_line).collect();
 
-    let acc = process_insts(&insts, 1);
+    let mut sprite_pos_map: HashMap<i32, i32> = HashMap::new();
+    let (acc, _) = process_insts(&insts, 1, &mut sprite_pos_map);
     println!("Signal accumulated: {:?}", acc);
+    let results: Vec<bool> = calculate_pixels(40, 6, &sprite_pos_map);
+    draw_screen(&results, 40);
     Ok(())
 }
